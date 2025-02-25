e501cbb66e7bd85cf4e05a8e44beea08cfa9583e
Alectrifying13 <apages1551@inst.hcpss.org>
2025-02-25 15:31:52 +0000

    cuppies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,32 @@
-package org.example; //DO NOT DELETE
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double gallons;
+        double quarts;
+        double pints;
+        double cups;
+        double tablespoons;
+        System.out.print("Enter the number of gallons: ");
+        gallons = input.nextDouble();
+        System.out.println("In " + gallons + " gallons there are: ");
+        quarts = gallons * 4;
+        System.out.println(quarts + " quarts");
+        pints = quarts * 2;
+        System.out.println(pints + " pints");
+        cups = pints * 2;
+        System.out.println(cups + " cups");
+        tablespoons = cups * 16;
+        System.out.println(tablespoons + " tablespoons");
+        
+
+
+
+
+
+
+    }
+}
